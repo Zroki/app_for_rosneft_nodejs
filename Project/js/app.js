a6b4044e209c9f356d03e5dd44f4031deb4c528e
Zroki <52646802+Zroki@users.noreply.github.com>
2020-11-03 14:17:26 +0500

    подписал функцию123123123132 (#2)
    Co-authored-by: Zroki <277869ac315>

--- Project/js/app.js
+++ Project/js/app.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector('#on');
 const num1 = document.querySelector('#num1');
 const num2 = document.querySelector('#num2');
-
+// qweqweq
 btn.addEventListener('click', () => {
   if (num1.value !== '' && num2.value !== '') {
     document.querySelector('#answer').innerText = +num1.value + +num2.value;
